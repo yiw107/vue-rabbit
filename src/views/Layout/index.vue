4485337ab85613aaaa13fec5956e6b5e2f622330
yiw107 <1458286231@qq.com>
2024-05-22 10:37:12 +0800

    吸顶导航交互实现

--- src/views/Layout/index.vue
+++ src/views/Layout/index.vue
@@ -2,9 +2,12 @@
 import LayoutNav from './components/LayoutNav.vue'
 import LayoutHeader from './components/LayoutHeader.vue'
 import LayoutFooter from './components/LayoutFooter.vue'
+
+import LayoutFixedVue from './components/LayoutFixed.vue'
 </script>
 
 <template>
+  <LayoutFixedVue/>
   <LayoutNav />
   <LayoutHeader />
   <RouterView />

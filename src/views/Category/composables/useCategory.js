b8fca5d069390784d17a442701f7fc0b5c71971c
yiw107 <1458286231@qq.com>
2024-05-22 22:21:30 +0800

    一级分类-按照业务逻辑拆分ha函数

---
+++ src/views/Category/composables/useCategory.js
@@ -0,0 +1,23 @@
+//封装分类数据业务相关代码
+import { onMounted ,ref} from "vue"
+import {getCategoryAPI} from '@/apis/category'
+import {onBeforeRouteUpdate } from 'vue-router'
+import {useRoute} from 'vue-router'
+export function useCategory(){
+   //获取数据
+const categoryData =ref({})
+const route = useRoute()//然后通过route就可以获取route参数了
+const getCategory = async(id=route.params.id)=>{
+    const res = await getCategoryAPI(id)
+    categoryData.value = res.result
+}
+onMounted(()=>getCategory()) 
+//目标 路由参数变化时 可以把分类数据接口重新发送
+onBeforeRouteUpdate((to)=>{
+    console.log('路由变化了')
+    //存在问题：使用最新的路由参数请求最新的分类数据
+  
+    getCategory(to.params.id)
+  })
+return {categoryData}
+}
--- src/views/Category/index.vue
+++ src/views/Category/index.vue
@@ -1,41 +1,14 @@
 <script setup>
-import {getCategoryAPI} from '@/apis/category'
-import {getBannerAPI} from '@/apis/home'
-import { onMounted, ref } from 'vue'
-import {onBeforeRouteUpdate, useRoute} from 'vue-router'
+
+import {useBanner} from './composables/useBanner'
+
 import GoodsItem from '../Home/components/GoodsItem.vue'
+import {useCategory} from './composables/useCategory'
 // import {onBeforeRouteUpdate} from 'vue-router'
+//解构赋值bannerList和categoryData
+const {bannerList} = useBanner()
+const {categoryData} = useCategory()
 
-//获取数据
-const categoryData =ref({})
-const route = useRoute()//然后通过route就可以获取route参数了
-const getCategory = async(id=route.params.id)=>{
-    const res = await getCategoryAPI(id)
-    categoryData.value = res.result
-}
-onMounted(()=>getCategory())
-
-//目标 路由参数变化时 可以把分类数据接口重新发送
-onBeforeRouteUpdate((to)=>{
-  console.log('路由变化了')
-  //存在问题：使用最新的路由参数请求最新的分类数据
-
-  getCategory(to.params.id)
-})
-
-
-// 获取banner
-const bannerList = ref([])
-
-const getBanner = async () => {
-  const res = await getBannerAPI({
-    distributionSite: '2'
-  })
-  
-  bannerList.value = res.result
-}
-
-onMounted(() => getBanner())
 </script>
 
 <template>

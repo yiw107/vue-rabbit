715c471d915f89c58050dead3b5ec8b1b568bf0b
yiw107 <1458286231@qq.com>
2024-05-25 11:38:52 +0800

    登录-token失效401拦截处理

--- src/utils/http.js
+++ src/utils/http.js
@@ -3,6 +3,7 @@ import axios from "axios";
 import { ElMessage } from 'element-plus'
 import 'element-plus/theme-chalk/el-message.css'
 import {useUserStore} from '@/stores/user'
+import {useRouter} from 'vue-router'
 const httpInstance=axios.create({
     baseURL:'http://pcapi-xiaotuxian-front-devtest.itheima.net',
     timeout:5000
@@ -22,11 +23,20 @@ httpInstance.interceptors.request.use(config => {
   
   // axios响应式拦截器
   httpInstance.interceptors.response.use(res => res.data, e => {
+    const userStore = useUserStore()
+    const router = useRouter()
     //统一错误提示
     ElMessage({
       type:'warning',
       message:e.response.data.message
     })
+    //401token失效处理
+    //1.清除本地用户数据
+    //2。跳转到登录页
+    if(e.response.status){
+      userStore.clearuserInfo()
+      router.push('/login')
+    }
     return Promise.reject(e)
   })
 export default httpInstance

85aee723259a83866d570df55222e2b9922b2835
yiw107 <1458286231@qq.com>
2024-05-26 21:56:57 +0800

    接口购物车-加入购物车

--- src/stores/cartStore.js
+++ src/stores/cartStore.js
@@ -1,12 +1,26 @@
 //封装购物车模块
 import {defineStore} from 'pinia'
 import { computed, ref } from 'vue'
+import {useUserStore} from './user'
+import { insertCartAPI,findNewCartListAPI } from '@/apis/cart'
+
+
 export const useCartStore = defineStore('cart',()=>{
+    const userStore = useCartStore()
+    const isLogin = computed(()=>userStore.userInfo.token)//islogin的值为true或者false，token表示是否是登陆状态
     //1.定义state-cartlist
     const cartList = ref([])
     //2.定义action-addcart
-    const addCart =()=>{
-        //添加购物车操作
+    const addCart =async(goods)=>{
+        const {skuId,count}=goods
+        if(isLogin.value){
+            //登录之后的加入购物车逻辑
+            await insertCartAPI({skuId,count})//加入购物车接口调用
+            const res = await findNewCartListAPI()//获取最新的购物车列表
+            cartList.value=res.result//覆盖本地的购物车列表
+        }else{
+            //本地逻辑
+            //添加购物车操作
         //已添加 则count+1
         //没有添加 直接push
         //思路 通过匹配传递过来的商品
@@ -18,6 +32,8 @@ export const useCartStore = defineStore('cart',()=>{
             //没找到
             cartList.value.push(goods)
         }
+        }
+        
     }
     //删除购物车
     const delCart =(skuId) =>{
